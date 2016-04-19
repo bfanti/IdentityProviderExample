259c038fea6abfceb701e30dc753e8a7d2cf8ad4
Bernardo Fanti <bfanti@peaksware.com>
2016-04-19 12:49:40 -0600

    Make it work with the latest DotNetCore RC2 build

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -1,18 +1,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace IdentityProvider.Controllers
 {
-    public class IdentityController : Controller
+    public class IdentityController
     {
     	public IdentityController() {}
 
-    	// GET: /<controller>/
-    	public IActionResult Index()
+		[HttpGet("")]
+    	public IActionResult GetIdentities()
     	{
-    		return new JsonResult(new {});
+    		return new JsonResult(new { Name = "Bernardo"});
     	}
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,47 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿using System.Reflection;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 
-namespace AspNetCoreCliDemo
+namespace IdentityProvider
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-        	services.AddMvc();
+        	services.AddMvcCore()
+        		    .AddJsonFormatters();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-        	app.UseDeveloperExceptionPage();
-        	app.UseStaticFiles();
-        	app.UseMvcWithDefaultRoute();
-        }
-
-        // This doesn't work right now, as it can't resolve WebApplication type
-        //public static void Main(string[] args) => WebApplication.Run<Startup>(args);
-
-        // Entry point for the application.
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                        .UseDefaultHostingConfiguration(args)
-                        .UseIISPlatformHandlerUrl()
-                        .UseKestrel()
-                        .UseStartup<Startup>()
-                        .Build();
-
-            host.Run();
+			app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller}/{action}");
+            });
         }
     }
 }	
